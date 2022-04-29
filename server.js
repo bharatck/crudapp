3b93b59f4f16cb08e85ada215e4c5103ce67ddef
bharatck <kbharat7@gmail.com>
2022-04-29 15:37:46 +0530

    fix

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
@@ -8,7 +7,6 @@ const connectDB = require("./server/database/connection");
 
 const app = express();
 
-dotenv.config({ path: "config.env" });
 const PORT = process.env.PORT || 3000;
 
 // log requests
